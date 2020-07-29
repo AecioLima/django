a4ba9ee10e41d7f2f4cf49ae1d46999786107358
AecioLima <64162776+AecioLima@users.noreply.github.com>
2020-07-29 20:44:18 -0300

    Update settings.py

--- django2/settings.py
+++ django2/settings.py
@@ -139,3 +139,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
